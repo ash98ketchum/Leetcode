c21994590252656d2ba2878cac227b45a046c298
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2025-04-02 14:36:15 +0530

    Time: 20 ms (75.29%), Space: 51.6 MB (7.3%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,19 +1,50 @@
 class Solution {
+    // public static int myrec(int idx1,int idx2, String text1, String text2, int dp[][]){
+    //     if (idx1 < 0 || idx2 < 0) {
+    //         return 0;
+    //     }
+    //     if (text1.charAt(idx1) == text2.charAt(idx2)) {
+    //         return dp[idx1][idx2]=1 + myrec(idx1 - 1, idx2 - 1, text1, text2, dp);
+    //     }
+    //     if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+    //     int cnt1 = myrec(idx1 - 1, idx2, text1, text2,dp);
+    //     int cnt2 = myrec(idx1, idx2 - 1, text1, text2,dp);
+    //     return dp[idx1][idx2]= Math.max(cnt1, cnt2);
+        
+    // }
     public int longestCommonSubsequence(String text1, String text2) {
-        int n1=text1.length();
-        int n2=text2.length();
-        int dp[][]=new int[n1+1][n2+1];
-        for(int i=1;i<=n1;i++){
-            for(int j=1;j<=n2;j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+1;
+        int n1 = text1.length();
+        int n2 = text2.length();
+        int dp[][] = new int[n1][n2];
+        if(text1.charAt(0) == text2.charAt(0)){
+            dp[0][0] = 1;
+        }
+        for(int i = 1; i < n1; i++){
+            if(text1.charAt(i) == text2.charAt(0)){
+                dp[i][0] = 1;
+            }else{
+                dp[i][0] = dp[i-1][0];
+            }
+        }
+        for(int j = 1; j < n2; j++){
+            if(text1.charAt(0) == text2.charAt(j)){
+                dp[0][j] = 1;
+            }
+            else{
+                dp[0][j] = dp[0][j-1];
+            }
+            
+        }
+        for(int i = 1; i < n1; i++){
+            for(int j = 1; j < n2; j++){
+                if(text1.charAt(i) == text2.charAt(j)){
+                    dp[i][j] = dp[i-1][j-1] +1;
                 }
                 else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-        return dp[n1][n2];
+        return dp[n1-1][n2-1];
     }
-
 }
