59564f0e0a64448584c73eccee4212ad2088774b
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2025-09-25 09:35:57 +0530

    Time: 3 ms (79.02%), Space: 43.9 MB (99.98%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,17 +1,16 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-       //state - > minimum path sum to a point (i,j) from 0,0 starting point by going either i, i+1,
-       //transition ->  dp[i][j] = a[i][j] + Min(dp[i-1][j-1], dp[i-1][j]);
-       // base condition -> dp[0][0] = a[0][0];
-       // final subproblem -> min(all the last rows) -> min(dp[n-1][j])
+        //state -> minimum path sum upto(i,j) from the top the triangle
+        //transition -> 
 
+        // base -> dp[0][0] = triangle[0][0];
+        // final problem -> dp[0][0];
         int n = triangle.size();
         int dp[][] = new int[n][n];
-
         dp[0][0] = triangle.get(0).get(0);
 
-        for(int i = 1; i < n ; i++){
-            for(int j = 0; j <= i; j++){
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= i ; j++){
                 if(j > 0 && j < i){
                     dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i-1][j], dp[i-1][j-1]);
                 } 
@@ -21,16 +20,14 @@ class Solution {
                 else {
                     dp[i][j] = triangle.get(i).get(j) + dp[i-1][j-1];
                 }
-                    
             }
         }
-
+        
         int ans = Integer.MAX_VALUE;
         for(int i = 0; i < n; i++){
             ans = Math.min(ans, dp[n-1][i]);
         }
         return ans;
-
 
     }
 }
