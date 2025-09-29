21f3b1f4fd42d338d2231bd7e128ec3b4246fa3a
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2025-09-29 10:57:46 +0530

    Time: 0 ms (100%), Space: 18.4 MB (99.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,25 +1,51 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size();
-        vector<bool> rows(n, false), cols(m, false);
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(matrix[i][j] == 0){
-                    rows[i] = true;
-                    cols[j] = true;
+        bool first_row_has_zero = false;
+        bool first_column_has_zero = false;
+        for(int i = 0;i<matrix.size(); ++i){
+            if(matrix[i][0]==0){
+                first_column_has_zero = true;
+                break;
+            }
+        }
+        for(int i = 0;i<matrix[0].size(); ++i){
+            if(matrix[0][i]==0){
+                first_row_has_zero = true;
+                break;
+            }
+        }
+        for(int i = 1; i<matrix.size(); ++i){
+            for(int j = 1; j<matrix[0].size(); ++j){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(rows[i] || cols[j]){
-                    matrix[i][j] = 0;
+        for(int i = 1;i<matrix.size(); ++i){
+            if(matrix[i][0]==0){
+                for(int j = 1; j<matrix[0].size(); ++j){
+                    matrix[i][j]=0;
                 }
             }
         }
-        
+        for(int j = 1;j<matrix[0].size(); ++j){
+            if(matrix[0][j]==0){
+                for(int i = 1; i<matrix.size(); ++i){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(first_row_has_zero){
+            for(int j = 0; j<matrix[0].size(); ++j){
+                matrix[0][j]=0;
+            }
+        }
+        if(first_column_has_zero){
+            for(int i = 0; i<matrix.size(); ++i){
+                matrix[i][0]=0;
+            }
+        }
     }
 };
