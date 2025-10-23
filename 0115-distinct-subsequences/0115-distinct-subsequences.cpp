d4f17adf98af959f6e59e6662998af586baf5bc3
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2025-10-23 22:46:25 +0530

    Time: 11 ms (89.4%), Space: 9.1 MB (81.9%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,21 +1,25 @@
-class Solution {
+class Solution{
 public:
-    int myrec(string &s, string &t, int i, int j, vector<vector<int>> &dp) {
-        if(j == t.size()) return 1;
-        if(i == s.size()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int x = 0, y = 0;
-        if(s[i] == t[j]) {
-            x = myrec(s, t, i + 1, j + 1, dp);
-        }
-        y = myrec(s, t, i + 1, j, dp);
-
-        return dp[i][j] = x + y;
-    }
-
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
-        return myrec(s, t, 0, 0, dp);
+        int n = s.size();
+        int m = t.size();
+        
+        vector<unsigned long long> prev(m + 1, 0);
+        vector<unsigned long long> curr(m + 1, 0);
+
+        prev[0] = 1; 
+        
+        for (int i = 1; i <= n; i++) {
+            curr[0] = 1;
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    curr[j] = prev[j - 1] + prev[j];
+                } else {
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
     }
 };
