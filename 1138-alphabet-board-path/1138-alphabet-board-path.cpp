1de4ad7bdd36c8054f49751a2c90854cd279041a
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2025-12-22 15:50:31 +0530

    Time: 6 ms (1.95%), Space: 11.9 MB (5.19%) - LeetHub

--- 1138-alphabet-board-path/1138-alphabet-board-path.cpp
+++ 1138-alphabet-board-path/1138-alphabet-board-path.cpp
@@ -1,44 +1,67 @@
 class Solution {
 public:
-    struct Node{
+    struct Node {
         int first;
         int second;
         string str;
-        Node(){} : (int)first, (int)second, (string)str;
+        Node(int f, int s, string st) {
+            first = f;
+            second = s;
+            str = st;
+        }
     };
+
     string alphabetBoardPath(string target) {
-        vector<string> grid = {"abcde", "fghij", "klmno", "pqrst", "uvwxy", "z...."};
+        vector<string> grid = {
+            "abcde",
+            "fghij",
+            "klmno",
+            "pqrst",
+            "uvwxy",
+            "z...."
+        };
+
         int n = grid.size();
         int m = grid[0].size();
 
         int sr = 0, sc = 0;
+        int dx[] = {0, -1, 0, 1};
+        int dy[] = {-1, 0, 1, 0};
+        char move[] = {'L', 'U', 'R', 'D'};
 
-        int dx[] = {-1,0,1,0};
-        int dy[] = {0,1,0,-1};
-        char move[] = {'L', 'U', 'R', 'D'}
         string ans = "";
-        for(auto t : target){
-            queue<Node> q;
-            q.push({sr,sc,""});
 
-            while(!q.empty()){
-                int r = q.front().first;
-                int c = q.front().second;
-                int str = q.front().str;
+        for (char t : target) {
+            queue<Node> q;
+            vector<vector<int>> vis(n, vector<int>(m, 0));
+
+            q.push(Node(sr, sc, ""));
+            vis[sr][sc] = 1;
+
+            while (!q.empty()) {
+                Node cur = q.front();
                 q.pop();
-                if(grid[r][c] == t){
-                    ans += str;
+
+                int r = cur.first;
+                int c = cur.second;
+                string path = cur.str;
+
+                if (grid[r][c] == t) {
+                    ans += path;
                     ans += '!';
                     sr = r;
                     sc = c;
                     break;
                 }
 
-                for(int i = 0; i < n; i++){
+                for (int i = 0; i < 4; i++) {
                     int nr = r + dx[i];
                     int nc = c + dy[i];
-                    if(nr >= 0 && nc >= 0 && nr < n && nc < m){
-                        q.push({nr, nc, str + move[i]});
+
+                    if (nr >= 0 && nc >= 0 && nr < n && nc < m &&
+                        grid[nr][nc] != '.' && !vis[nr][nc]) {
+                        vis[nr][nc] = 1;
+                        q.push(Node(nr, nc, path + move[i]));
                     }
                 }
             }
