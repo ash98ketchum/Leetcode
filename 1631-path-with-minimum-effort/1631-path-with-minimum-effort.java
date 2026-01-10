fd72a24f1f092662fb5c184314dba4577edd63e7
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2026-01-10 15:23:53 +0530

    Time: 82 ms (13.93%), Space: 48.3 MB (6.69%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,51 +1,52 @@
 class Solution {
-    class tuple{
-        int r;
-        int c;
-        int dist;
-        tuple(int r,int c, int dist){
-            this.r = r;
-            this.c = c;
-            this.dist = dist;
+    public boolean check(int mid, int mat[][]){
+        boolean vis[][] = new boolean[mat.length][mat[0].length];
+        return dfs(mid, mat, 0,0, vis);
+        
+    }
+    public boolean dfs(int mid, int mat[][], int i, int j, boolean vis[][]){
+        int n = mat.length;
+        int m = mat[0].length;
+        if(i == mat.length - 1 && j == mat[0].length -1){
+            return true;
         }
+        vis[i][j] = true;
+        
+        // i -1 j
+        if(i - 1 >= 0 && Math.abs(mat[i - 1][j] - mat[i][j]) <= mid && !vis[i - 1][j]){
+            if(dfs(mid, mat, i - 1, j, vis)){
+                return true;
+            }
+        }
+        if(i + 1 < n && Math.abs(mat[i + 1][j] - mat[i][j]) <= mid && !vis[i + 1][j]){
+            if(dfs(mid, mat, i + 1, j, vis)){
+                return true;
+            }
+        }
+        if(j - 1 >= 0 && Math.abs(mat[i][j - 1] - mat[i][j]) <= mid && !vis[i][j - 1]){
+            if(dfs(mid, mat, i, j - 1,  vis)){
+                return true;
+            }
+        }
+        if(j + 1 < m && Math.abs(mat[i][j + 1] - mat[i][j]) <= mid && !vis[i][j + 1]){
+            if(dfs(mid, mat, i, j + 1, vis)){
+                return true;
+            }
+        }
+        return false;
     }
     public int minimumEffortPath(int[][] heights) {
-        int n = heights.length;
-        int m = heights[0].length;
-
-        int dx[] = {-1,0,1,0};
-        int dy[] = {0,1,0,-1};
-
-        int dist[][] = new int[n][m];
-        for(int it[] : dist){
-            Arrays.fill(it, Integer.MAX_VALUE);
-        }
-        PriorityQueue<tuple> pq = new PriorityQueue<>((a,b)-> Integer.compare(a.dist, b.dist));
-        dist[0][0] = 0;
-        pq.offer(new tuple(0,0,0));
-
-        while(!pq.isEmpty()){
-            int r = pq.peek().r;
-            int c = pq.peek().c;
-            int dis = pq.peek().dist;
-            pq.poll();
-            if(r == n - 1 && c == m -1){
-                return dis;
-            }
-            for(int i = 0; i < 4; i++){
-                int newr = r + dx[i];
-                int newc = c + dy[i];
-                if(newr >=0 && newr < n && newc >=0 && newc < m){
-                    int neweffort = Math.abs(heights[newr][newc] - heights[r][c]);
-                    neweffort = Math.max(neweffort, dis);
-                    if(neweffort < dist[newr][newc]){
-                        dist[newr][newc] = neweffort;
-                        pq.offer(new tuple(newr, newc, neweffort));
-                    }
-                    
-                }
+        int l = 0, h = Integer.MAX_VALUE;
+        int ans = -1;
+        while(l <= h){
+            int mid = l + (h- l)/2;
+            if(check(mid, heights)){
+                ans = mid;
+                h = mid - 1;
+            }else{
+                l = mid + 1;
             }
         }
-        return 0;
+        return ans;
     }
 }
