e578f2eab8646f11a01b766fef54cfff994ce7ac
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2026-01-13 12:17:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (18.92%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int ans = 0;
+        while((int)pow((minutesToTest/minutesToDie + 1), ans) < buckets) ans++;
+        return ans;
+    }
+};
