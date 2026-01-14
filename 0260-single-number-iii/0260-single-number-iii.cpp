7f7ca20c5ad497e68ffd002c2c3d473423e842a4
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2026-01-14 12:31:19 +0530

    Time: 0 ms (100%), Space: 13.8 MB (96.39%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -3,19 +3,15 @@ public:
     vector<int> singleNumber(vector<int>& nums) {
         int xr = 0;
         for(auto it : nums) xr ^= it;
-        int b = 0;
-        while((xr & 1) == 0){
-            b++;
-            xr /= 2;
-        }
+        //1001000 , 
         int x = 0;
         int y = 0;
-        int mask = (1 << b);
+        unsigned int ux = (unsigned int)xr;
+        unsigned int mask = ux & (-ux);
         for(auto it : nums){
             if((it & mask) == 0) x ^= it;
             else y ^= it;
         }
-        vector<int> ans = {x, y};
-        return ans;
+        return {x, y};
     }
 };
