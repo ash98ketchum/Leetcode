84290e0839c3cac9b4e072b76b35b66774798611
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2026-01-19 14:08:02 +0530

    Time: 3 ms (35.8%), Space: 77.1 MB (26.82%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int minNumberOperations(vector<int>& target) {
-        int n = target.size();
-        int ans = target[0];
-        for (int i = 1; i < n; i++) {
-            if (target[i] > target[i - 1]) {
-                ans += target[i] - target[i - 1];
+        int sum = target[0];
+        for(int i = 1; i < target.size(); i++){
+            if(target[i] > target[i - 1]){
+                sum += (target[i] - target[i - 1]);
             }
         }
-        return ans;
+        return sum;
     }
 };
