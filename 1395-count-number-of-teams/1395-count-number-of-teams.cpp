d449c91ddee44651d3cee503eb7ad8ae31375137
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2026-01-20 23:34:30 +0530

    Time: 23 ms (67.42%), Space: 12.5 MB (54.24%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int n = rating.size();
+        int ans = 0;
+
+        for (int j = 1; j < n - 1; j++) {
+            int leftLess = 0, leftGreater = 0;
+            int rightLess = 0, rightGreater = 0;
+
+            for (int i = 0; i < j; i++) {
+                if (rating[i] < rating[j]) leftLess++;
+                else if (rating[i] > rating[j]) leftGreater++;
+            }
+
+            for (int k = j + 1; k < n; k++) {
+                if (rating[k] < rating[j]) rightLess++;
+                else if (rating[k] > rating[j]) rightGreater++;
+            }
+
+            ans += leftLess * rightGreater + leftGreater * rightLess;
+        }
+
+        return ans;
+    }
+};
