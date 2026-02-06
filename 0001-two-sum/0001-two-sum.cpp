48ad7876b96728eb2959fb387a3cf4b3be9ba134
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2026-02-06 22:22:03 +0530

    Time: 0 ms (100%), Space: 14.8 MB (36.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // bruteforce
-
+        unordered_map<int, int> mp;
         for(int i = 0; i < nums.size(); i++){
-            for(int j = i + 1; j < nums.size(); j++){
-                if((nums[i] + nums[j]) == target){
-                    return {i,j};
-                }
+            int complement = target - nums[i];
+            if(mp.find(complement) != mp.end()){
+                return {mp[complement], i};
             }
+            mp[nums[i]] = i;
         }
-        return {}; // this will never execute
-
+        return {-1, -1};
     }
 };
