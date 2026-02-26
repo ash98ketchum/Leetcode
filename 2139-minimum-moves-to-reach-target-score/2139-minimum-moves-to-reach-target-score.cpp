b54180755ea149552ea857be0fabd78cea8effea
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2026-02-26 10:19:38 +0530

    Time: 0 ms (100%), Space: 7.8 MB (95.36%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minMoves(int target, int maxDoubles) {
+        int steps = 0;
+        
+        while(target > 1 && maxDoubles > 0){
+            if(target % 2 == 0) {
+                target /= 2;
+                maxDoubles--;
+            }
+            else{
+                target -= 1;
+            }
+            steps++;
+        }
+        if(target > 1){
+            steps += (target - 1);
+        }        
+        return steps;
+    }
+};
