25fea770569bcc5c3f5a429553256f1932ea44ae
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2026-04-17 23:38:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
+++ LeetCode/Medium/3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
+    int reve(int n){
+        int ans = 0;
+        while(n > 0){
+            ans = ans * 10 + n % 10;
+            n/=10;
+        }
+        return ans;
+    }
     int minMirrorPairDistance(vector<int>& nums) {
-        
+        map<int, int> mp;
+        int dist = 1e9;
+        for(int i = 0; i < nums.size(); i++){
+            int curr = nums[i];
+
+            if(mp.count(curr)){
+                int j = mp[curr];
+                int d2 = i - j;
+                if(d2 < dist){
+                    dist = d2;
+                }
+                
+            }
+            int rev = reve(curr);
+            mp[rev] = i;
+        }
+        if(dist == 1e9) return -1;
+        return dist;
     }
 };
