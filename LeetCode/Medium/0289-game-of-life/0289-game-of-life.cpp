b2110188106c25ae22ce120a508d8f2b210a905a
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2026-04-28 22:20:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0289-game-of-life/0289-game-of-life.cpp
+++ LeetCode/Medium/0289-game-of-life/0289-game-of-life.cpp
@@ -3,7 +3,6 @@ public:
     void gameOfLife(vector<vector<int>>& board) {
         int n = board.size();
         int m = board[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, 0));
 
         int dx[] = {1, 1, 1, 0, 0, -1, -1, -1};
         int dy[] = {-1, 1, 0, 1, -1, 0, -1, 1};
@@ -14,21 +13,24 @@ public:
                 for(int k = 0; k < 8; k++){
                     int ni = i + dx[k];
                     int nj = j + dy[k];
-                    if(ni >= 0 && nj >= 0 && ni < n && nj < m && board[ni][nj] == 1){
+                    if(ni >= 0 && nj >= 0 && ni < n && nj < m && (board[ni][nj] == 1 || board[ni][nj] == 2)){
                         cnt1++;
                     }
                 }
 
                 if(board[i][j] == 1){
-                    if(cnt1 >3 || cnt1 < 2) ans[i][j] = 0;
-                    if(cnt1 == 2 || cnt1 == 3) ans[i][j] = 1;
+                    if(cnt1 < 2 || cnt1 > 3) board[i][j] = 2; 
                 }
-                else{
-                    if(cnt1 == 3) ans[i][j] = 1;
+                else if(board[i][j] == 0) {
+                    if(cnt1 == 3) board[i][j] = 3;
                 }
-
             }
         }
-        board = ans;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == 2) board[i][j] = 0;
+                else if(board[i][j] == 3) board[i][j] = 1; 
+            }
+        }
     }
 };
