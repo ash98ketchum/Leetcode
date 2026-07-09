f659c480ded893f2e6435783845060d1c84f41f8
Anirudh Chauhan <anirudhchauhan8074@gmail.com>
2026-07-09 09:19:28 +0530

    Time: 0 ms (100%), Space: 31.2 MB (38.33%) - LeetHub

---
+++ LeetCode/Easy/3978-unique-middle-element/3978-unique-middle-element.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isMiddleElementUnique(vector<int>& nums) {
+        int n = nums.size();
+        int ele = nums[n/2];
+        for(int i = 0; i < n; i++) if(i != n/2 && ele == nums[i]) return false;
+        return true;
+    }
+};
